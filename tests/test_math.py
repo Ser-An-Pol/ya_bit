74c43beadf481ec62a0e0116a8c2f7de11bf7558
Ser-An-Pol <seranpol@icloud.com>
2026-03-17 12:41:32 +0300

    Format code

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,21 +1,18 @@
-""" import pytest
+import pytest
 
 
 def add_sum(a, b):
     return a + b
 
 
-@pytest.mark.parametrize("param_set", [
-    (2, 3),
-    (3, 4),
-    (5, 7)
-], indirect=True)
+@pytest.mark.parametrize("param_set", [(2, 3), (3, 4), (5, 7)], indirect=True)
 def test_add(param_set):
-    assert add_sum(param_set[0], param_set[1]) == param_set[2], \
-        'сумма не равна ожидаемой'
+    assert (
+        add_sum(param_set[0], param_set[1]) == param_set[2]
+    ), "сумма не равна ожидаемой"
+
 
 @pytest.mark.type
 def test_type_result():
     assert isinstance(add_sum(2, 3), int), \
-        'не соответствует ожидаемому типу данных' """
-    
+        "не соответствует ожидаемому типу данных"
--- tests/test_stock_market_data.py
+++ tests/test_stock_market_data.py
@@ -1,33 +1,39 @@
 import pytest
 
 
-@pytest.mark.parametrize("get_marker", [
-    pytest.param('tokenId', marks=pytest.mark.token),
-    pytest.param('currencyId', marks=pytest.mark.currency),
-    pytest.param('count_offers', marks=pytest.mark.count_offers),
-    pytest.param('valid_price', marks=pytest.mark.valid_price),
-    pytest.param('valid_quantity', marks=pytest.mark.valid_price),
-])
+@pytest.mark.parametrize(
+    "get_marker",
+    [
+        pytest.param("tokenId", marks=pytest.mark.token),
+        pytest.param("currencyId", marks=pytest.mark.currency),
+        pytest.param("count_offers", marks=pytest.mark.count_offers),
+        pytest.param("valid_price", marks=pytest.mark.valid_price),
+        pytest.param("valid_quantity", marks=pytest.mark.valid_price),
+    ],
+)
 def test_offer(get_data, get_marker):
     match get_marker:
-        case 'tokenId':
+        case "tokenId":
             for offer in get_data[1]:
-                assert get_data[0] == offer['tokenId'], \
-                    f'Wrong token: {offer['tokenId']} \
-                        instead {get_data[0]}'
-        case 'currencyId':
+                assert (
+                    get_data[0] == offer["tokenId"]
+                ), f"Wrong token: {offer['tokenId']} \
+                        instead {get_data[0]}"
+        case "currencyId":
             for offer in get_data[1]:
-                assert 'RUB' == offer['currencyId'], \
-                    f'Wrong currency: {offer['currencyId']} instead RUB'
-        case 'count_offers':
-            assert len(get_data[1]) == 10, \
-                f'Wrong count of offers: {len(get_data[1])} instead 10'
-        case 'valid_price':
+                assert (
+                    "RUB" == offer["currencyId"]
+                ), f"Wrong currency: {offer['currencyId']} instead RUB"
+        case "count_offers":
+            assert (
+                len(get_data[1]) == 10
+            ), f"Wrong count of offers: {len(get_data[1])} instead 10"
+        case "valid_price":
             for offer in get_data[1]:
-                assert float(offer['price']) > 0, \
-                    'Wrong price! It must be positive'
-        case 'valid_quantity':
+                assert float(offer["price"]) > 0, \
+                    "Wrong price! It must be positive"
+        case "valid_quantity":
             for offer in get_data[1]:
-                assert float(offer['quantity']) > 0, \
-                    'Wrong quantity! It must be positive'
-            
+                assert (
+                    float(offer["quantity"]) > 0
+                ), "Wrong quantity! It must be positive"
